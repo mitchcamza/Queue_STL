d75d015e18287a610ad4bbf38caa4959b14af6e7
mitchcamza <tide.mute_06@icloud.com>
2022-11-04 12:01:39 +0200

    implemented isInLanguageL using STL queue

--- Queue_STL/main.cpp
+++ Queue_STL/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stack>
 #include <queue>
+#include <string>
 
 template <class Type>
 void reverseQ(std::queue<Type> &q)
@@ -32,15 +33,60 @@ void printQueue(std::queue<Type> &theQueue)
 	std::cout << std::endl;
 }
 
+
+bool isInLanguageL(std::string w)
+{
+	std::queue<char> q;
+	unsigned int index = 0;
+	
+	while (w[index] == 'a')
+	{
+		q.emplace('-');
+		index++;
+	}
+	
+	while (w[index] == 'b' && !q.empty())
+	{
+		if (q.front() == '-')
+		{
+			q.pop();
+			index++;
+		}
+	}
+	
+	if (q.empty())
+	{
+		return index == w.length();
+	}
+	
+	else
+	{
+		return false;
+	}
+}
+
 int main()
 {
-	std::queue<int> q;
-	q.emplace(1);
-	q.emplace(2);
-	q.emplace(3);
-	q.emplace(4);
-	reverseQ(q);
-	printQueue(q);
+//	std::queue<int> q;
+//	q.emplace(1);
+//	q.emplace(2);
+//	q.emplace(3);
+//	q.emplace(4);
+//	reverseQ(q);
+//	printQueue(q);
+	
+	
+	std::string w = "aabb";
+	
+	if (isInLanguageL(w))
+	{
+		std::cout << w << " is in the language L" << std::endl;
+	}
+	
+	else
+	{
+		std::cout << w << " is not in the language L" << std::endl;
+	}
 	
 	return 0;
 }
