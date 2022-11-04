6c30990f8be4ac763c456f60067672c9c8b2a2c0
mitchcamza <tide.mute_06@icloud.com>
2022-11-04 07:44:05 +0200

    Initial Commit

---
+++ Queue_STL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Queue_STL
+//
+//  Created by Mitch Campbell on 2022/11/04.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
