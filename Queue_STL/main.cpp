583f7e194865e0401728719dbb8b704687750a9a
mitchcamza <192mitch88@gmail.com>
2023-01-10 06:20:13 +0200

    Implemented and tested replaceEverySecondItem for std::queue

--- Queue_STL/main.cpp
+++ Queue_STL/main.cpp
@@ -28,6 +28,7 @@ void printQueue(const std::queue<Type>& theQueue)
 {
 	// create a copy of the queue to maintain original queue elements
 	std::queue<Type> tempQueue = theQueue;
+	
 	while (!tempQueue.empty())
 	{
 		std::cout << tempQueue.front() << " ";
@@ -68,12 +69,42 @@ bool isInLanguageL(std::string w)
 	}
 }
 
+// Function replaces every second item in a queue with a new item.
+template <class Type>
+void replaceEverySecondItem(std::queue<Type>& q, const Type& newItem)
+{
+	// create a temp queue to maintain original elements
+	std::queue<Type> tempQueue;
+	
+	// move elements to temp queue
+	while (!q.empty())
+	{
+		tempQueue.emplace(q.front());
+		q.pop();
+	}
+	
+	// conditionally move elements back to original queue
+	unsigned int currentIndex = 0;
+	while (!tempQueue.empty())
+	{
+		if (currentIndex % 2 == 0)
+		{
+			q.emplace(tempQueue.front());
+		}
+		else
+		{
+			q.emplace(newItem);
+		}
+		tempQueue.pop();
+		currentIndex++;
+	}
+}
+
 static void test_reverseQ()
 {
 	std::queue<int> emptyQueue;
 	reverseQ(emptyQueue);
 	assert(emptyQueue.empty());
-	
 	
 	/// test with a queue of size 5
 	std::queue<int> q;
@@ -103,10 +134,30 @@ static void test_reverseQ()
 	printQueue(q);
 }
 
+static void test_replaceEverySeconItem()
+{
+	std::queue<int> myQueue;
+	
+	// add elements to queue
+	myQueue.emplace(1);
+	myQueue.emplace(3);
+	myQueue.emplace(4);
+	myQueue.emplace(6);
+	myQueue.emplace(7);
+	myQueue.emplace(8);
+	myQueue.emplace(2);
+	
+	std::cout << "My queue before replaceEverySecondItem: ";
+	printQueue(myQueue);
+	
+	replaceEverySecondItem(myQueue, 0);
+	std::cout << "My queue after replaceEverySecondItem: ";
+	printQueue(myQueue);
+}
+
 int main()
 {
-	/// test with an empty queue
-	test_reverseQ();
+	test_replaceEverySeconItem();
 	
 	return 0;
 }
